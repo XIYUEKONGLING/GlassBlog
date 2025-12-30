9022d05f0669bd5edbd8d22ec1948fc1ada55121
XIYUEKONGLING <229533969+XIYUEKONGLING@users.noreply.github.com>
2025-12-30 20:35:06 +0800

    Add ANOTHER_IDENTITY to Profile and extend consts with new fields

--- src/consts.ts
+++ src/consts.ts
@@ -3,13 +3,19 @@
 
 
 // Website Settings
-export const SITE_TITLE = 'Glass Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Glass Blog";
+export const SITE_DESCRIPTION = "Welcome to my website!";
 
 
 // Another Profile Settings
-export const ANOTHER_NAME = 'XiYueNYA';
+export const ANOTHER_NAME = "XiYueNYA";
 export const ANOTHER_AVATAR = "/avatar.png";
 export const ANOTHER_AVATAR_QUALITY = 80;
 
-export const ANOTHER_DESCRIPTION : string | null = 'Full-Stack Developer & Blogger';
+export const ANOTHER_DESCRIPTION : string | null = "Full-Stack Developer & Blogger";
+
+export const ANOTHER_IDENTITY : string | null = "@XIYUEKONGLING";
+export const ANOTHER_PRONOUNS : string | null = "She/Her";
+
+export const ANOTHER_LOCATION: string | null = "ZheJiang, China";
+export const ANOTHER_TIMEZONE: string = "Asia/Shanghai";
