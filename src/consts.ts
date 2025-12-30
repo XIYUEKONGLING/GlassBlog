0c2c0fcdccb9e6f153434361f52a71ee13b9ce38
XIYUEKONGLING <229533969+XIYUEKONGLING@users.noreply.github.com>
2025-12-30 19:42:07 +0800

    Add Profile component with avatar, name, and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,15 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
+
+// Website Settings
 export const SITE_TITLE = 'Glass Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+
+// Another Profile Settings
+export const ANOTHER_NAME = 'XiYueNYA';
+export const ANOTHER_AVATAR = "/avatar.png";
+export const ANOTHER_AVATAR_QUALITY = 80;
+
+export const ANOTHER_DESCRIPTION : string | null = 'Full-Stack Developer & Blogger';
